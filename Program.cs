b935b7737d568576b158ecb13139614b99b2e8fc
Marwan <Marwan.kaseer1997@gmail.com>
2022-02-15 22:45:47 +0300

    Program is done

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] isimler ; int n; float yas = 0;
+            Console.Write("lutfen calisanlarin sayisi giriniz: ");
+            n = int.Parse(Console.ReadLine());
+            isimler = new int[n];
+            for (int i=0; i<n; i++)
+            {
+                Console.Write("Lutfen {0}.calisanin yasini giriniz: ", i+1);
+                isimler[i] = int.Parse(Console.ReadLine());
+            }
+            foreach (var j in isimler)
+            {
+                yas += j;
+            }
+            Console.WriteLine("calisanlarin yaslarinin ortalamasi " + yas/n);
+        }
+    }
+}
